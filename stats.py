31eaa75616d6710bf1752b3f47ce6387dc62f437
Tim Menzies <tim@menzies.us>
2014-04-05 08:33:23 -0400

    adas

--- stats.py
+++ stats.py
@@ -1,46 +1,64 @@
-import zipfile,re
+import zipfile,re,os,fnmatch
 
-def atom(x):
-  try : return int(x)
-  except ValueError:
-    try : return float(x)
-    except ValueError : return x
+def values(str,  sep=",",
+           bad= r'(["\' \t\r\n]|#.*)'):
+  def value(x):
+    try : return int(x)
+    except ValueError:
+      try : return float(x)
+      except ValueError : return x
+  return map(value,re.sub(bad,"",str).split(sep))
 
-def atoms(str, 
-          sep=",",
-          bad= r'(["\' \t\r\n]|#.*)'):
-  return map(atom,re.sub(bad,"",str).split(sep))
+def interpolate(x, points):
+  lo, hi = points[0], points[-1]
+  x1,y1  = lo[0], lo[1]
+  for x2,y2 in points[1:]:
+    if x1 <= x <= x2:
+      deltay = y2 - y1
+      deltax = (x- x1)*1.0/(x2- x1)
+      return y1 + deltay * deltax
+    x1,y1 = x2,y2
+  return hi[1]
 
-def identity(*lst): 
-  return lst[0] if len(lst)==1 else lst
-
-def unzip(zipped):
+def zippedFiles(zipped):
+  "Find files in a zip."
   with zipfile.ZipFile(zipped,'r') as archive:
     for file in archive.namelist():
       yield file,archive.open(file,'r')
 
-def namedZipCells(zipped):
-  for file,lines in unzip(zipped):
-    line, names = None, None
+def files(pattern):
+  "Find files in a directory tree."
+  for path, subdirs, files in os.walk('.'):
+    for name in files:
+      if fnmatch.fnmatch(name, pattern):
+        yield path + '/' + name,\
+              open(os.path.join(path, name))
+
+def namedCells(source, filter=files):
+  "Return cells, with header info from line1."
+  for file,lines in filter(source):
+    names = None
     for line in lines:
-      cells = atoms(line)
+      cells = values(line)
       if not names: 
         names = cells
       else: 
         yield file,zip(names,cells) 
    
-def wantgot(zipped,f=identity):
-  for file,found in namedZipCells(zipped):
+def wantgot(source,compare=lambda x,y:x - y, 
+            filter=files):
+  "Return cells, compared to values in first col."
+  for file,found in namedCells(source,filter):
     want = found[0][1]
     for what,got in found[1:]:
-      yield file,what,f(want,got)
+      yield file, what, compare(want,got)
 
 def ar(want,got)    : return want - got
 def mar(want,got)   : return abs(ar(want,got))
 def relerr(want,got): return (want-got)*1.0/want
 def mre(want,got)   : return abs(relerr(want,got))
 
-class Total():
+class Nums():
   def __init__(i,some=[]):
     i.n = i.mu = i.m2 = i.s = 0.0; i.all=[]
     for x in some: i % x
@@ -51,7 +69,7 @@ class Total():
     i.mu  += delta*1.0/i.n
     i.m2  += delta*(x - i.mu)
     if i.n > 1: i.s = 1.0*(i.m2/(i.n - 1))**0.5
-  def __add__(i,j): return Total(i.all + j.all)
+  def __add__(i,j): return Nums(i.all + j.all)
   def t(i,j):
     signal = abs(i.mu - j.mu)*1.0
     noise  = (i.s**2/i.n + j.s**2/j.n)**0.5
@@ -59,45 +77,40 @@ class Total():
   def ttest(i,j,conf=95):
     return critical(i.n + j.n - 2,conf) < i.t(j)
 
-def critical(n, conf=95,
-             ts= {95: ((  1, 12.70 ), ( 3, 3.182),
-                       (  5,  2.571), (10, 2.228),
-                       ( 20,  2.086), (80, 1.99 ),
-                       (320,  1.97 )),
-                  99: ((  1, 63.657), ( 3, 5.841),
-                       (  5,  4.032), (10, 3.169),
-                       ( 20,  2.845), (80, 2.64 ),
-                       (320,  2.58 ))}):
-  confs  = ts[conf]
-  lo, hi = confs[0], confs[-1]
-  n1,t1  = lo[0], lo[1]
-  for n2,t2 in confs:
-    if n1 <= n <= n2:
-      return t1+ (t2- t1)* (n- n1)*1.0/(n2- n1)
-    n1,t1 = n2,t2
-  return hi[1]
+def critical(n, conf=95):
+  return interpolate(n,
+                     {95:((  1, 12.70 ), ( 3, 3.182),
+                          (  5,  2.571), (10, 2.228),
+                          ( 20,  2.086), (80, 1.99 ),
+                          (320,  1.97 )),
+                      99:((  1, 63.657), ( 3, 5.841),
+                          (  5,  4.032), (10, 3.169),
+                          ( 20,  2.845), (80, 2.64 ),
+                          (320,  2.58 ))}[conf])
 
 def _t():
   one = [105,112,96,124,103,92,97,108,105,110]
   two = [98,108,114,106,117,118,126,116,122,108]
   for want,fudge in [(False,1.0), (False,1.1), 
                      (True,1.2),  (True,9.0)]:
-    t1  = Total(two)
-    t2  = Total(map(lambda x:x*fudge, one))
+    t1  = Nums(two)
+    t2  = Nums(map(lambda x:x*fudge, one))
     got = t1.ttest(t2)
     print fudge,":testPassed",want == got,\
           "since :want",want,":got",got
 
+_t()
+
 def _demo(zipped='data/loo.zip'):
   wme = {}
-  for file,rx,seen in wantgot(zipped,mre):
+  for file,rx,seen in wantgot(zipped,mre,zippedFiles):
     key = rx
     if not key in wme: 
-      wme[key] = Total()
+      wme[key] = Nums()
     wme[key] % seen
   for k1,v1 in wme.items():
     for k2,v2 in wme.items():
       if k1 > k2:
         print k1,k2,v1.ttest(v2)
 
-_demo()
+#_demo()
