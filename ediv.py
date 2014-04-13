19057f19037c135fa05b41b0ff4a7c0480725315
Tim Menzies <tim@menzies.us>
2014-04-13 11:09:40 -0400

    asdas

--- ediv.py
+++ ediv.py
@@ -1,38 +1,6 @@
-"""
-#<
-ediv: entropy-based division of numerics .
-Copyright (c) 2014, Tim Menzies, tim.menzies@gmail.com
-All rights reserved.  
-
-This code implements the Fayyad and Irani MDL discretizer of numerics as
-described in Supervised and Unsupervised Discretization of Continuous Features,
-James Dougherty, Ron Kohavi, Mehran Sahami, ICML'95
-  
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
+#ediv: entropy-based division of numerics .
+#Copyright (c) 2014, Tim Menzies, tim.menzies@gmail.com
+#All rights reserved.  
 
 import sys,random
 sys.dont_write_bytecode = True
@@ -66,8 +34,8 @@ def ediv(lst, tiny=2,
   def ecut(this): # Find best divide of 'this' lst.
     def ke(z): return z.k()*z.ent()
     lhs,rhs   = Counts(),Counts(sym(x) for x in this)
-    n0,k0,e0,ke0 = rhs.n, rhs.k(), rhs.ent(), ke(rhs)
-    cut, least   = None, e0
+    n0,k0,e0,ke0= 1.0*rhs.n,rhs.k(),rhs.ent(),ke(rhs)
+    cut, least  = None, e0
     for j,x  in enumerate(this): 
       if lhs.n > tiny and rhs.n > tiny: 
         maybe= lhs.n/n0*lhs.ent()+ rhs.n/n0*rhs.ent()
