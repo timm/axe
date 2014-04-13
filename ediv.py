6dd53b2e67b68c19b3e807e9c26939152f1baa17
Tim Menzies <tim@menzies.us>
2014-04-12 22:51:39 -0400

    asdas

--- ediv.py
+++ ediv.py
@@ -1,8 +1,14 @@
 """
 #<
-ediv: entropy-based division of numerics
+ediv: entropy-based division of numerics .
 Copyright (c) 2014, Tim Menzies, tim.menzies@gmail.com
 All rights reserved.  
+
+This code implements the Fayyad and Irani MDL
+discretizer of numerics as described in Supervised
+and Unsupervised Discretization of Continuous
+Features, James Dougherty, Ron Kohavi, Mehran
+Sahami, ICML'95
   
 Redistribution and use in source and binary forms,
 with or without modification, are permitted provided
@@ -41,64 +47,77 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###################################################
 """
 
-import sys,math
+import sys,math,random
 sys.dont_write_bytecode = True
 log=math.log
+bell=random.gauss
+seed=random.seed
+
+def log2(x) : return log(x,2)
 
 class Counts():
   "Place to add/delete counts of symbols."
   def __init__(i,inits=[]):
-    i.n = 0
+    i.n, i._e = 0, None
     i.cache = {}
     for symbol in inits:  i + symbol
-  def __add__(i,symbol) :
-    i.n += 1
-    i.cache[symbol] = i.cache.get(symbol,0) + 1
-  def __sub__(i,symbol) : 
-    i.n -= 1
-    i.cache[symbol] = i.cache.get(symbol,0) - 1
+  def __add__(i,symbol) : i.inc(symbol,  1)
+  def __sub__(i,symbol) : i.inc(symbol, -1)
+  def inc(i,symbol,n=1):
+    i._e = None
+    i.n += n
+    i.cache[symbol] = i.cache.get(symbol,0) + n
+  def k(i) : return len(i.cache.keys())
+  def ke(i): return i.k()*i.ent()
   def ent(i):
-    e = 0
-    for symbol in i.cache:
-      p  = i.cache[symbol]*1.0/i.n
-      if p:
-        e -= p*log(p)*1.0/log(2)
-    return e
+    if i._e == None:
+      i._e = 0
+      for symbol in i.cache:
+        p  = i.cache[symbol]*1.0/i.n
+        if p:
+          i._e -= p*log2(p)*1.0
+    return i._e
 
-def ediv(pairs,gets,cuts):
+def ediv(pairs,num,sym):
   "Divide pairs of (numbers,symbols) using entropy."
-  cut,e = ecut(pairs,gets)
-  if cut:
-    ediv(pairs[:cut], gets, cuts)
-    ediv(pairs[cut:], gets, cuts)
-  else:
-    cuts += [(e,pairs)]
-  return cuts
- 
-def ecut(pairs,(num,sym),min=3):
+  def recurse(pairs):
+    cut,e = ecut(pairs,num,sym)
+    if cut:
+      recurse(pairs[:cut])
+      recurse(pairs[cut:])
+    else:
+      cuts.append((e,pairs))
+    return cuts
+  cuts = []
+  if pairs:
+    return recurse(sorted(pairs,key=num))
+
+def ecut(pairs,num,sym):
   "Find best place to divide pairs of (num,sym)."
-  cut,least= None,None
-  left     = Counts()
-  right    = Counts(sym(x) for x in pairs)
-  n        = len(pairs) * 1.0
-  least    = right.ent()
+  n   = len(pairs) * 1.0
+  lhs = Counts()
+  rhs = Counts(sym(x) for x in pairs)
+  k, e, ke = rhs.k(), rhs.ent(), rhs.ke()
+  cut,min  = None,e
   for j,x  in enumerate(pairs):
-    n1,n2 = left.n, right.n
-    if n1 > min and n2 > min:
-      tmp = n1/n*left.ent() + n2/n*right.ent()
-      if tmp < least :
-        cut,least = j,tmp
-    right - sym(x)
-    left  + sym(x)    
-  return cut,least
+    maybe = lhs.n/n*lhs.ent() + rhs.n/n*rhs.ent()
+    if maybe < min :  
+      gain = e - maybe
+      delta= log2(3**k-2)- (ke- rhs.ke()- lhs.ke())
+      if gain >= (log2(n-1) + delta)/n: 
+        cut,min = j,maybe
+    rhs - sym(x)
+    lhs + sym(x)    
+  return cut,min
 
 def _ediv():
   "Demo code to test the above."
-  def first(x) : return x[0]
-  def second(x) : return x[1]
+  seed(1)
+  def num(x) :  return x[0]
+  def sym(x) : return x[1]
   def go(lst):
-    print ""; print lst
-    for d in  ediv(sorted(lst,key=first),(first,second),[]):
+    print ""; print lst[:10]
+    for d in  ediv(lst,num,sym):
       print d[1][0][0]
   X,Y="X","Y"
   go([(1,X),(2,X),(3,X),(4,X),(11,Y),(12,Y),(13,Y),(14,Y)])
@@ -107,6 +126,13 @@ def _ediv():
   go([(64,X),(65,Y),(68,X),(69,Y),(70,X),(71,Y),
       (72,X),(72,Y),(75,X),(75,X),
       (80,Y),(81,Y),(83,Y),(85,Y)])
+  l=[]
+  for _ in range(1000): 
+    l += [(bell(20,1),  X),(bell(10,1),Y),
+          (bell(30,1),'Z'),(bell(40,1),'W')] 
+  go(l)
+  go([(1,X)])
+  
 
 _ediv()
 
@@ -118,21 +144,29 @@ Output:
 1
 11
 
-[(1, 'Y'), (2, 'X'), (3, 'X'), (4, 'X'),
+[(1, 'Y'), (2, 'X'), (3, 'X'), (4, 'X'), 
  (11, 'Y'), (12, 'Y'), (13, 'Y'), (14, 'Y')]
 1
-11
 
 [(1, 'X'), (2, 'X'), (3, 'X'), (4, 'X'), 
  (11, 'X'), (12, 'X'), (13, 'X'), (14, 'X')]
 1
 
-
-[(64, 'X'), (65, 'Y'), (68, 'X'), (69, 'Y'), (70, 'X'), (71, 'Y'), 
- (72, 'X'), (72, 'Y'), (75, 'X'), (75, 'X'), 
- (80, 'Y'), (81, 'Y'), (83, 'Y'), (85, 'Y')]
+[(64, 'X'), (65, 'Y'), (68, 'X'), (69, 'Y'), 
+ (70, 'X'), (71, 'Y'), (72, 'X'), (72, 'Y'), 
+ (75, 'X'), (75, 'X')]
 64
-72
-80
-"""
 
+[(21.288184753155463, 'X'), (11.44944560869977, 'Y'), 
+ (30.066335808938263, 'Z'), (39.23545634902837, 'W'), 
+ (18.90782678489586, 'X'), (10.031334516831716, 'Y'),
+  (28.977896829989128, 'Z'), (38.56317055489747, 'W'), 
+ (20.199311976483752, 'X'), (10.133374604658606, 'Y')]
+6.90037812106
+16.907507693
+26.850034984
+36.8600218357
+
+[(1, 'X')]
+1
+"""
