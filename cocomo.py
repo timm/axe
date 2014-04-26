a8ecd6b4ea59c82ee447378ebbbbb46ee1593c7d
Tim Menzies <tim@menzies.us>
2014-04-26 00:16:24 -0400

    asdsa

--- cocomo.py
+++ cocomo.py
@@ -133,7 +133,7 @@ def tdev(it):
 def f(it):
   """f is the exponent used in the tdev function.  It is calculated from the
    constants d and b, and the scale exponent used in the pmNs function.  """
-   return d(it)+0.2*(e(it)-b(it))
+  return d(it)+0.2*(e(it)-b(it))
 
 def pm(it):
   """pm is the person-month calculation, the amount of time one person spends 
@@ -145,12 +145,12 @@ def pm(it):
 def pmNs(it):
   """ pmNs is the amount of effort in person-months. pmNs is calculated from the 
   constant a(), size(), and the scale exponent, e(), and the following values. """
-  return a(it)*(size(it)^e(it))*               \
+  return ats(it,['a']) * (size(it)^e(it))*               \
       prod(ats(it,['rely','data','cplx','ruse',  
                    'docu','time','stor','pvol',
                    'acap','pcap','pcon','apex''plex',
                    'ltex', 'tool', 'site']))
-
+ 
 def ats(it,lst):
   t = tunings()
   def x(str)      : return it[str].x()
@@ -158,37 +158,39 @@ def ats(it,lst):
   return [tuning(str) for str in lst]
           
 def prod(lst):
-  out=1:
+  out=1
   for x in lst: out *= x
   return out
 
 def e(it) :
-    """e is the scale exponent used in the pmNs function.  It calculated from
+  """e is the scale exponent used in the pmNs function.  It calculated from
   the constant b and the percent result of summing the selected scale
   scale factors"""
-   return b(it)+0.01*sum(ats(it,['prec','flex','resl','team','pmat']))
+  return b(it)+0.01*sum(ats(it,['prec','flex','resl','team','pmat']))
 
-def tunings(_ = None):
-  return {
-    ﬂex:  [5.07, 4.05, 3.04, 2.03, 1.01,    _],
-    pmat: [7.80, 6.24, 4.68, 3.12, 1.56,    _],
-    prec: [6.20, 4.96, 3.72, 2.48, 1.24,    _],
-    resl: [7.07, 5.65, 4.24, 2.83, 1.41.    _]
-    team: [5.48, 4.38, 3.29, 2.19, 1.01,    _], 
-    acap: [1.42, 1.19, 1.00, 0.85, 0.71,    _], 
-    aexp: [1.22, 1.10, 1.00, 0.88, 0.81,    _], 
-    cplx: [0.73, 0.87, 1.00, 1.17, 1.34, 1.74], 
-    data: [   _, 0.90, 1.00, 1.14, 1.28,    _], 
-    docu: [0.81, 0.91, 1.00, 1.11, 1.23,    _], 
-    ltex: [1.20, 1.09, 1.00, 0.91, 0.84,    _], 
-    pcap: [1.34, 1.15, 1.00, 0.88, 0.76,    _], 
-    pcon: [1.29, 1.12, 1.00, 0.90, 0.81,    _], 
-    plex: [1.19, 1.09, 1.00, 0.91, 0.85,    _], 
-    pvol: [   _, 0.87, 1.00, 1.15, 1.30,    _], 
-    rely: [0.82, 0.92, 1.00, 1.10, 1.26,    _], 
-    ruse: [   _, 0.95, 1.00, 1.07, 1.15, 1.24], 
-    sced: [1.43, 1.14, 1.00, 1.00, 1.00,    _], 
-    site: [1.22, 1.09, 1.00, 0.93, 0.86, 0.80], 
-    stor: [   _,    _, 1.00, 1.05, 1.17, 1.46], 
-    time: [   _,    _, 1.00, 1.11, 1.29, 1.63], 
-    tool: [1.17, 1.09, 1.00, 0.90, 0.78,    _]}
+def tunings( _ = None):
+  return dict( 
+    flex= [5.07, 4.05, 3.04, 2.03, 1.01,    _ ],
+    pmat= [7.80, 6.24, 4.68, 3.12, 1.56,    _],
+    prec= [6.20, 4.96, 3.72, 2.48, 1.24,    _],
+    resl= [7.07, 5.65, 4.24, 2.83, 1.41,    _],
+    team= [5.48, 4.38, 3.29, 2.19, 1.01,    _], 
+    acap= [1.42, 1.19, 1.00, 0.85, 0.71,    _], 
+    aexp= [1.22, 1.10, 1.00, 0.88, 0.81,    _], 
+    cplx= [0.73, 0.87, 1.00, 1.17, 1.34, 1.74], 
+    data= [   _, 0.90, 1.00, 1.14, 1.28,    _], 
+    docu= [0.81, 0.91, 1.00, 1.11, 1.23,    _], 
+    ltex= [1.20, 1.09, 1.00, 0.91, 0.84,    _], 
+    pcap= [1.34, 1.15, 1.00, 0.88, 0.76,    _], 
+    pcon= [1.29, 1.12, 1.00, 0.90, 0.81,    _], 
+    plex= [1.19, 1.09, 1.00, 0.91, 0.85,    _], 
+    pvol= [   _, 0.87, 1.00, 1.15, 1.30,    _], 
+    rely= [0.82, 0.92, 1.00, 1.10, 1.26,    _], 
+    ruse= [   _, 0.95, 1.00, 1.07, 1.15, 1.24], 
+    sced= [1.43, 1.14, 1.00, 1.00, 1.00,    _], 
+    site= [1.22, 1.09, 1.00, 0.93, 0.86, 0.80], 
+    stor= [   _,    _, 1.00, 1.05, 1.17, 1.46], 
+    time= [   _,    _, 1.00, 1.11, 1.29, 1.63], 
+    tool= [1.17, 1.09, 1.00, 0.90, 0.78,    _])
+
+print estimate()
