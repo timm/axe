0de0f214663d037c7824849ae76d266bb3f0540a
timm <tim@menzies.us>
2014-08-27 08:33:40 -0400

    stuff

--- hansel/hansel.py
+++ hansel/hansel.py
@@ -2,7 +2,28 @@ from __future__ import division
 import sys, random
 sys.dont_write_bytecode = True
 
+### Place to store things and stuff ################
+class Thing(object):
+  def __init__(i,**d): i.__dict__.update(d)
+
+### Place to store options #########################
+
+The = Thing(cache= Thing(keep=128))
+
+### Misc utils #####################################
 rand= random.random
+
+# seperate d sub-lists from print sublists
+def hasd(d):
+  name = d.__class__.__name__
+  d=  d  if isinstance(d,dict) else d.__dict__
+  for k in sorted(d.keys):
+    val = k[d]
+    if isinstance(val,(dict,Thing)):
+      s += showd(d,lvl+1)
+    else:
+      s += '\n#   ' * lvl + (':%s %v' % (k,d[k]))
+  return s
 
 def g3(lst): return gn(lst,3)
 def g0(lst): return gn(lst,0)
@@ -11,24 +32,22 @@ def gn(lst,n):
   fmt = '%.' + str(n) + 'f'
   return ', '.join([(fmt % x) for x in sorted(lst)])
 
-class Thing(object):
-  def __init__(i,**fields): i.override(fields)
-  def also(i,**d)         : return i.override(d)
-  def override(i,d): i.__dict__.update(d); return i
-
-The = Thing(cache= Thing(keep=128))
+### Classes ########################################
 
 class Sample(Thing):
   "Keep a random sample of stuff seen so far."
-  def __init__(i,inits=[]):
+  def __init__(i,inits=[],nump=True):
     i._cache,i.n,i._also = [],0,None
-    i.lo, i.hi = 10**32, -10**32
+    i.nump = nump
+    if i.nump:
+      i.lo, i.hi = 10**32, -10**32
     for n in inits: i.__iadd__(n)
   def __iadd__(i,x):
-    i.n += 1
-    i.lo = min(x,i.lo)
-    i.hi = max(x,i.hi)
     changed = False
+    i.n += 1
+    if i.nump:
+      i.lo = min(x,i.lo)
+      i.hi = max(x,i.hi)
     if len(i._cache) < The.cache.keep:
       changed = True
       i._cache += [x]               # then add
@@ -59,11 +78,10 @@ class In:
     i.txt,i.lo,i.hi = txt,lo,hi
   def __call__(i): 
     return i.lo+(i.hi - i.lo)*rand()
-    
+
 class It:
   def __init__(i, indep=None, dep=None):
-    i.indep = [] if indep==None else indep
-    i.dep   = [] if dep  ==None else dep
+    i.indep, i.dep = indep,dep
   def __repr__(i):
     return ':indep %s :dep %s' % (
            g3(i.indep),g3(i.dep))
@@ -73,31 +91,42 @@ class Model:
     i.of = i.spec()
     i.log= It(indep=[Sample() for _ in i.of.indep],
                dep =[Sample() for _ in i.of.dep])
-  def guess(i):
-    return It([r() for r in i.of.indep])
-  def score(i,one):
-    one.dep = [f(one) for f in i.of.dep]
-  def seen(i,one):
-    for val,log in zip(one, i.log):
+  def indepIT(i):
+    "Make new it."
+    return It([f() for f in i.of.indep])
+  def depIT(i,it):
+    "Complete it's dep variables."
+    it.dep = [f(it) for f in i.of.dep]
+  def logIT(i,it):
+    "Remember what we have see in it."
+    for val,log in zip(it, i.log):
       if val != None:
         log += val
+  def aroundIT(i,it,p=0.5):
+    "Find some place around it."
+    def n(val,f): 
+      return f() if rand() < p else x
+    old = it.indep
+    new = [n(val,f) for val,f in zip(old,i.of.indep)]
+    return It(indep=new, dep=old.dep)
 
-#It = indep,dep pair
+#It = a class describing indep,dep pair
 
-#one = actual values
-#of = meta knowledge of members of one
-#log = a record of things seen in one
+#it = actual values
+#of = meta knowledge of members of it
+#log = a record of things seen in it
 
 class ZDT1(Model):
   def spec(i):
     return It(indep= [In(0,1,x) for x in range(30)],
               dep  = [i.f1,i.f2])
-  def f1(i,one):
-    return one.indep[0]
-  def f2(i,one):
-    return 1 + 9*sum(one.indep[1:]) / 29
+  def f1(i,it):
+    return it.indep[0]
+  def f2(i,it):
+    return 1 + 9*sum(it.indep[1:]) / 29
 
 z= ZDT1()
-one= z.guess()
-z.score(one)
-print one
+for _ in xrange(10000):
+  it= z.indepIT()
+  z.depIT(it)
+  #print it
