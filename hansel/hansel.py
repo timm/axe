323e690c1f62a366b5907dc7d2f54baf5430a743
timm <tim@menzies.us>
2014-08-28 01:05:05 -0400

    stuff

--- hansel/hansel.py
+++ hansel/hansel.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import sys, random.math
+import sys, random,math
 sys.dont_write_bytecode = True
 
 ### Place to store things and stuff ################
@@ -8,26 +8,41 @@ class Thing:
 
 ### Place to store options #########################
 
-The = Thing(cache= Thing(keep=128))
+The = Thing(cache= Thing(keep    = 128,
+                         pending = 4),
+            sa   = Thing(cooling = 0.6,
+                         kmax    = 1000,
+                         epsilon = 1.01,
+                         verbose = True,
+                         era     = 50,
+                         baseline = 100))
 
 ### Misc utils #####################################
 rand= random.random
 any=  random.choice
-def log2(x): return math.log(x,2)
+def log2(x): return math.log(x)/math.log(2)
+def say(x): 
+  sys.stdout.write(str(x)); sys.stdout.flush()
 
-# seperate d sub-lists from print sublists
-def hasd(d): # dont print _vars. if function, print name
-  name = d.__class__.__name__
-  d=  d  if isinstance(d,dict) else d.__dict__
-  for k in sorted(d.keys):
-    val = k[d]
+def showd(d,lvl=0): 
+  d = d if isinstance(d,dict) else d.__dict__
+  after, line,gap = [], '', '\t' * lvl
+  for k in sorted(d.keys()):
+    if k[0] == "_": continue
+    val = d[k]
     if isinstance(val,(dict,Thing)):
-      s += showd(d,lvl+1)
+       after += [k]
     else:
-      s += '\n#   ' * lvl + (':%s %v' % (k,d[k]))
-  return s
+      if callable(val):
+        val = val.__name__
+      line += (':%s %s ' % (k,val))
+  print gap + line
+  for k in after: 
+      print gap + (':%s' % k)
+      showd(d[k],lvl+1)
 
 def g3(lst): return gn(lst,3)
+def g2(lst): return gn(lst,2)
 def g0(lst): return gn(lst,0)
 
 def gn(lst,n):
@@ -40,9 +55,9 @@ class Log(Thing):
   "Keep a random sample of stuff seen so far."
   def __init__(i,inits=[],label=''):
     i.label = label
-    i._cache,i.n,i._has = [],0,None
+    i._cache,i.n,i._report = [],0,None
     i.setup()
-    map(i.__add__,inits)
+    map(i.__iadd__,inits)
   def __iadd__(i,x):
     if x == None: return x
     i.n += 1
@@ -55,31 +70,32 @@ class Log(Thing):
         changed = True
         i._cache[int(rand()*The.cache.keep)] = x
     if changed:      
-      i._has = None # wipe out 'what follows'
+      i._report = None # wipe out 'what follows'
       i.change(x)
     return i
-  def __call__(i):  
+  def any(i):  
     return  any(i._cache)
   def has(i):
-    i._has = i._has or i.also()
-    return i._has
+    i._report = i._report or i.report()
+    return i._report
+  def setup(i): pass
 
-def Num(Log):
+class Num(Log):
   def setup(i):
     i.lo, i.hi = 10**32, -10**32
   def change(i,x):
-    i.lo = min(i.lo,x)
-    i.hi = max(i.hi,x)
-  def also(i):
+    i.lo = min(i.lo, x)
+    i.hi = max(i.hi, x)
+  def report(i):
     lst = i._cache = sorted(i._cache)
-    n   = len(i._cache)     
+    n   = len(lst)     
     return Thing(
-      median = i.median()
-      iqr    = lst[int(n*.75)] - lst[int(n*.5)],
-      lo     = i.lo, 
-      hi     = i.hi)
+      median= i.median(),
+      iqr   = lst[int(n*.75)] - lst[int(n*.5)],
+      lo    = i.lo, 
+      hi    = i.hi)
   def ish(i,f=0.1): 
-    return i() + f*(i() - i())
+    return i.any() + f*(i.any() - i.any())
   def median(i):
     n = len(i._cache)
     p = n // 2
@@ -92,16 +108,28 @@ class Sym(Log):
   def setup(i):
     i.counts,i.mode,i.most={},None,0
   def change(i,x):
-    n= i.counts[x] = i.counts.get(x,0) + 1
-    if n > i.most:
-      i.mode,i.most = x,n
-  def also(i):
-    return  Thing(counts = i.counts,
-                  ent    = i,entropy(),
-                  mode   = i.mode)
+    c= i.counts[x]= i.counts.get(x,0) + 1
+    if c > i.most:
+      i.mode,i.most = x,c
+  def report(i):
+     return Thing(dist= i.dist(), 
+                  ent = i.entropy(),
+                  mode= i.mode)
+  def dist(i):
+    n = sum(i.counts.values())
+    return sorted([(d[k]/n, k) for 
+                   k in i.counts.keys()], 
+                  reverse=True)
+  def ish(i):
+    r,tmp = rand(),0
+    for w,x in i.has().dist:
+      tmp  += w
+      if tmp >= r: 
+        return x
+    return x
   def entropy(i,e=0):
     for k in i.counts:
-      p = i.counts[k]/i.n
+      p = i.counts[k]/len(i._cache)
       e -= p*log2(p) if p else 0
     return e    
 
@@ -111,40 +139,40 @@ class In:
   def __init__(i,lo=0,hi=1,txt=""):
     i.txt,i.lo,i.hi = txt,lo,hi
   def __call__(i): 
-    return i.lo+(i.hi - i.lo)*rand()
-  def log(): return Num()
+    return i.lo + (i.hi - i.lo)*rand()
+  def log(i): 
+    return Num()
 
-class Of:
-  def __init__(i, indep=None, dep=None):
-    i.indep, i.dep = indep,dep
+class XY:
+  def __init__(i, x=None, y=None):
+    i.x, i.y = x,y
   def __repr__(i):
-    return ':indep %s :dep %s' % (
-           g3(i.indep),g3(i.dep))
+    return ', '.join(map(str,[':x',i.x,':y',i.y]))
 
 class Model:
   def __init__(i):
     i.of = i.spec()
-    i.log= Of(indep= [x.log() for x in i.of.indep],
-                dep= [Num()   for _ in i.of.dep])
+    i.log= XY(x= [z.log() for z in i.of.x],
+              y= [Num()   for _ in i.of.y])
   def indepIT(i):
     "Make new it."
-    return Of(indep=[f() for f in i.of.indep])
+    return XY(x=[z() for z in i.of.x])
   def depIT(i,it):
     "Complete it's dep variables."
-    it.dep = [f(it) for f in i.of.dep]
+    it.y = [f(it) for f in i.of.y]
   def logIT(i,it):
     "Remember what we have see in it."
     for val,log in zip(it, i.log):
       log += val
-  def aroundIT(i,it,p=0.33):
+  def aroundIT(i,it,p=0.5):
     "Find some place around it."
     def n(val,f): 
       return f() if rand() < p else val
-    old = it.indep
-    new = [n(x,f) for x,f in zip(old,i.of.indep)]
-    return Of(indep=new, dep=old.dep)
+    old = it.x
+    new = [n(x,f) for x,f in zip(old,i.of.x)]
+    return XY(x=new)
 
-#Of = a pair of related indep,dep lists
+#XY = a pair of related indep,dep lists
 
 #it = actual values
 #of = meta knowledge of members of it
@@ -155,15 +183,68 @@ class Model:
 
 class ZDT1(Model):
   def spec(i):
-    return Of(indep= [In(0,1,x) for x in range(30)],
-              dep  = [i.f1,i.f2])
+    return XY(x= [In(0,1,z) for z in range(30)],
+              y= [i.f1,i.f2])
   def f1(i,it):
-    return it.indep[0]
+    return it.x[0]
   def f2(i,it):
-    return 1 + 9*sum(it.indep[1:]) / 29
+    return 1 + 9*sum(it.x[1:]) / 29
 
-z= ZDT1()
-for _ in xrange(10000):
-  it= z.indepIT()
-  z.depIT(it)
-  #print it
+class Schaffer(Model):
+  def spec(i):
+    return XY(x= [In(-10,10,0)],
+              y= [i.f1,i.f2])
+  def f1(i,it):
+    x=it.x[0]; return x**2
+  def f2(i,it):
+    x=it.x[0]; return (x-2)**2
+
+def burp(x):  
+  The.sa.verbose and say(x)
+
+def sa(m):
+  return sa1(m, The.sa.epsilon,
+                The.sa.kmax,
+                The.sa.cooling,
+                The.sa.era,
+                The.sa.baseline)
+
+def sa1(m, epsilon,kmax,cooling,era,baseline):
+  def energy(it): 
+    m.depIT(it)
+    return sum(it.y)
+  def norm(e,lo,hi):
+    return 1 - (e - lo) / (hi - lo)
+  def candidate():
+    it = m.indepIT()
+    m.depIT(it)
+    return it
+  def maybe(old,new,temp): 
+    return math.e**(-1*(old - new)/temp) < rand()
+  def baseline():
+    es = []
+    for _ in xrange(The.sa.baseline):
+      it = m.indepIT()
+      es += [energy(it)]
+    es = sorted(es)
+    return es[0],es[-1]
+  lo,hi = baseline()
+  sb = s = m.indepIT()
+  eb = e = norm(energy(s),lo,hi)
+  print eb
+  for k in xrange(kmax):
+    k += 1
+    sn = m.aroundIT(s)
+    en = norm(energy(sn),lo,hi)
+    if en > (eb * epsilon):
+      sb,sb = sn,en; burp("!")
+    if en > (e *  epsilon):
+      s,e = sn,en; burp("+")
+    elif maybe(e,en, k/kmax):
+      s,e = sn,en; burp("?")
+    if not k % era: 
+      burp("\n" + str(eb) + " ")
+    burp(".")
+  print eb    
+
+print sa(Schaffer())
