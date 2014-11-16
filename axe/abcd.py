9c1778e6c1a8cf4e206f0cd5e6536ee091b39e7f
Tim Menzies <tim@menzies.us>
2014-11-16 11:00:24 -0500

    stuff

--- axe/abcd.py
+++ axe/abcd.py
@@ -66,4 +66,13 @@ def _Abcd():
   abcd.header()
   abcd.ask()
 
+"""
+output:
+ $ python abcd.py 
+# db                   rx           n     a    b    c   d    acc pd  pf  prec f  g  class     
+----------------------------------------------------------------------------------------------------
+# randomIn             jiggle       22    0    5    5   17   63  77 100  77  77   0 a         
+# randomIn             jiggle        5   17    5    5    0   63   0  23   0  77   0 b       
+"""
+
 if __name__ == "__main__": _Abcd()
